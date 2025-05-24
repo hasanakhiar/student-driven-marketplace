05333191a3c0eda4c2152d07c4442630b7f18f02
WasiOmar <48373736+WasiOmar@users.noreply.github.com>
2025-05-25 03:22:47 +0600

    Delete server directory

--- server/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require('mongoose')
-
-const connectDB =(url)=>{
-    return mongoose.
-    connect(url).then(()=> console.log('database connected'))
-    .catch((err)=>console.log(err))
-}
-
-module.exports = connectDB;
